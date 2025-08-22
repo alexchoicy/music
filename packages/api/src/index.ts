78f650792bc541f6186062b76ca3c3fd1765ebb1
alexchoicy <alex123321choicy@gmail.com>
2025-08-22 22:31:16 +0800

    init

---
+++ packages/api/src/index.ts
@@ -0,0 +1,1 @@
+//i am new, but this file is needed, and i dunno why
---
+++ packages/api/src/schemas/user.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  id: z.string().uuid(),
+  email: z.string().email(),
+  name: z.string().min(2).max(100),
+  age: z.number().min(0).optional(),
+});
+
+export type User = z.infer<typeof UserSchema>;
