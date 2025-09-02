4033a7c648ce4719f9c24cbd9e41503ed01fcdbf
alexchoicy <alex123321choicy@gmail.com>
2025-09-03 03:18:08 +0800

    init

---
+++ apps/web/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class=" w-full h-screen flex items-center justify-center px-4 ">
+    <AuthLogin></AuthLogin>
+  </div>
+</template>
---
+++ packages/ui/nuxt.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: [join(currentDir, './assets/css/tailwind.css')],
+  alias: {
+    '@': currentDir,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt', '@nuxtjs/color-mode'],
+  shadcn: {
+    prefix: '',
+    componentDir: join(currentDir, './components/ui')
+  },
+  colorMode: {
+    classSuffix: '',
+    preference: 'dark'
+  }
+})
