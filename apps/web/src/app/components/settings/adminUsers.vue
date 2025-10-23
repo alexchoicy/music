f2493820ca1ceae786f10a0785160e656c0306d8
alexchoicy <alex123321choicy@gmail.com>
2025-10-23 14:54:36 +0800

    feat(ui): finish create user

--- apps/web/src/app/components/settings/adminUsers.vue
+++ apps/web/src/app/components/settings/adminUsers.vue
@@ -1,126 +1,145 @@
 <script setup lang="ts">
 import { UserRolesSchema } from "@music/api/dto/auth.dto";
-import { createUserRequestSchema, type UsersInfoDTO } from "@music/api/dto/users.dto";
+import {
+  createUserRequestSchema,
+  type UsersInfoDTO,
+} from "@music/api/dto/users.dto";
 import { toTypedSchema } from "@vee-validate/zod";
 import { User, UserPlus, ShieldUser, Trash, Pencil } from "lucide-vue-next";
 import { useForm } from "vee-validate";
 
-const users = useAPI<UsersInfoDTO[]>('/users');
-
+const users = useAPI<UsersInfoDTO[]>("/users");
 
 const formSchema = toTypedSchema(createUserRequestSchema);
 
 const form = useForm({
-    validationSchema: formSchema
+  validationSchema: formSchema,
 });
 
 const onFormSubmit = form.handleSubmit(async (values) => {
-    console.log("Submitting form with values:");
-    console.log(values);
+  await useNuxtApp().$backend("users", {
+    method: "POST",
+    body: values,
+  });
+  users.refresh();
 });
 
 function formatDate(date: Date): string {
-    const d = new Date(date);
-    const day = String(d.getDate()).padStart(2, '0');
-    const month = String(d.getMonth() + 1).padStart(2, '0');
-    const year = d.getFullYear();
-    const hours = String(d.getHours()).padStart(2, '0');
-    const minutes = String(d.getMinutes()).padStart(2, '0');
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  const d = new Date(date);
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const year = d.getFullYear();
+  const hours = String(d.getHours()).padStart(2, "0");
+  const minutes = String(d.getMinutes()).padStart(2, "0");
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
-
 </script>
 
 <template>
-    <div class="space-y-6">
-        <Card>
-            <CardHeader>
-                <CardTitle>Add New User</CardTitle>
-                <CardDescription>
-                    Create new users
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form @submit="onFormSubmit">
-                    <!-- I think the field thingy is not working properly with form right now -->
-                    <!-- no error message -->
-                    <FieldGroup>
-                        <FieldSet>
-                            <div class="grid grid-cols-2 gap-4">
-                                <Field>
-                                    <FieldLabel for="username">Username</FieldLabel>
-                                    <FormField v-slot="{ componentField }" name="username">
-                                        <Input id="username" type="text" v-bind="componentField" required />
-                                    </FormField>
-                                </Field>
-                                <Field>
-                                    <FieldLabel for="displayName">Display Name</FieldLabel>
-                                    <FormField v-slot="{ componentField }" name="displayName">
-                                        <Input id="displayName" type="text" v-bind="componentField" required />
-                                    </FormField>
-                                </Field>
-                            </div>
-                            <Field>
-                                <FieldLabel for="password">Password</FieldLabel>
-                                <FormField v-slot="{ componentField }" name="password">
-                                    <Input id="password" type="password" v-bind="componentField" required />
-                                </FormField>
-                            </Field>
-                            <Field>
-                                <FieldLabel for="role">Role</FieldLabel>
-                                <FormField v-slot="{ componentField }" name="role">
-                                    <Select id="role" v-bind="componentField">
-                                        <SelectTrigger>
-                                            <SelectValue placeholder="Select a role" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectItem v-for="options in UserRolesSchema.options" :key="options"
-                                                :value="options">
-                                                {{ options }}
-                                            </SelectItem>
-                                        </SelectContent>
-                                    </Select>
-                                </FormField>
-                            </Field>
-                        </FieldSet>
+  <div class="space-y-6">
+    <Card>
+      <CardHeader>
+        <CardTitle>Add New User</CardTitle>
+        <CardDescription> Create new users </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form @submit="onFormSubmit">
+          <!-- I think the field thingy is not working properly with form right now -->
+          <!-- no error message -->
+          <FieldGroup>
+            <FieldSet>
+              <div class="grid grid-cols-2 gap-4">
+                <Field>
+                  <FieldLabel for="username">Username</FieldLabel>
+                  <FormField v-slot="{ componentField }" name="username">
+                    <Input
+                      id="username"
+                      type="text"
+                      v-bind="componentField"
+                      required
+                    />
+                  </FormField>
+                </Field>
+                <Field>
+                  <FieldLabel for="displayName">Display Name</FieldLabel>
+                  <FormField v-slot="{ componentField }" name="displayName">
+                    <Input
+                      id="displayName"
+                      type="text"
+                      v-bind="componentField"
+                      required
+                    />
+                  </FormField>
+                </Field>
+              </div>
+              <Field>
+                <FieldLabel for="password">Password</FieldLabel>
+                <FormField v-slot="{ componentField }" name="password">
+                  <Input
+                    id="password"
+                    type="password"
+                    v-bind="componentField"
+                    required
+                  />
+                </FormField>
+              </Field>
+              <Field>
+                <FieldLabel for="role">Role</FieldLabel>
+                <FormField v-slot="{ componentField }" name="role">
+                  <Select id="role" v-bind="componentField">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem
+                        v-for="options in UserRolesSchema.options"
+                        :key="options"
+                        :value="options"
+                      >
+                        {{ options }}
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormField>
+              </Field>
+            </FieldSet>
 
-                        <Field orientation="horizontal">
-                            <Button type="submit">Submit</Button>
-                            <Button variant="outline" type="button">
-                                Cancel
-                            </Button>
-                        </Field>
-                    </FieldGroup>
-                </form>
-            </CardContent>
-        </Card>
-        <Card>
-            <CardContent>
-                <Item v-for="user in users.data.value" :key="user.id">
-                    <ItemMedia>
-                        <User v-if="user.role === 'user'" />
-                        <ShieldUser v-else-if="user.role === 'admin'" />
-                        <UserPlus v-else-if="user.role === 'uploader'" />
-                        <User v-else />
-                    </ItemMedia>
-                    <ItemContent>
-                        <ItemTitle>{{ user.displayname }} ({{ user.username }})</ItemTitle>
-                        <ItemDescription>
-                            Role: {{ user.role }} | Created At: {{ formatDate(user.createdAt) }}
-                        </ItemDescription>
-                    </ItemContent>
-                    <ItemActions>
-                        <div class="grid grid-cols-2 gap-4">
-                            <Button variant="ghost" size="sm">
-                                <Pencil />
-                            </Button>
-                            <Button variant="ghost" size="sm">
-                                <Trash />
-                            </Button>
-                        </div>
-                    </ItemActions>
-                </Item>
-            </CardContent>
-        </Card>
-    </div>
+            <Field orientation="horizontal">
+              <Button type="submit">Submit</Button>
+              <Button variant="outline" type="button"> Cancel </Button>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+    <Card>
+      <CardContent>
+        <Item v-for="user in users.data.value" :key="user.id">
+          <ItemMedia>
+            <User v-if="user.role === 'user'" />
+            <ShieldUser v-else-if="user.role === 'admin'" />
+            <UserPlus v-else-if="user.role === 'uploader'" />
+            <User v-else />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>{{ user.displayname }} ({{ user.username }})</ItemTitle>
+            <ItemDescription>
+              Role: {{ user.role }} | Created At:
+              {{ formatDate(user.createdAt) }}
+            </ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <div class="grid grid-cols-2 gap-4">
+              <Button variant="ghost" size="sm">
+                <Pencil />
+              </Button>
+              <Button variant="ghost" size="sm">
+                <Trash />
+              </Button>
+            </div>
+          </ItemActions>
+        </Item>
+      </CardContent>
+    </Card>
+  </div>
 </template>
--- apps/web/src/app/pages/settings/index.vue
+++ apps/web/src/app/pages/settings/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Fingerprint } from 'lucide-vue-next';
+import { Fingerprint, Users } from 'lucide-vue-next';
 import { cn } from '~/lib/utils';
 
 const section = ref('webauth');
@@ -10,46 +10,46 @@ const isAdmin = computed(() => user?.value?.info.role === 'admin')
 </script>
 
 <template>
-    <div class="flex flex-col gap-8 lg:flex-row">
-        <nav class="w-full lg:w-64">
-            <div class="space-y-1">
-                <button :class="cn(
-                    'flex w-full items-start gap-3 rounded-lg px-4 py-3 text-left transition-colors hover:cursor-pointer',
-                    section === 'webauth'
-                        ? 'bg-primary text-primary-foreground'
-                        : 'hover:bg-accent hover:text-accent-foreground'
-                )" @click="section = 'webauth'">
-                    <Fingerprint class="mt-0.5 h-5 w-5 shrink-0" />
-                    <div class="flex-1 space-y-0.5">
-                        <div class="flex items-center gap-2">
-                            <span class="font-medium">
-                                Web Authentication
-                            </span>
-                        </div>
-                    </div>
-                </button>
-                <button v-if="isAdmin" :class="cn(
-                    'flex w-full items-start gap-3 rounded-lg px-4 py-3 text-left transition-colors hover:cursor-pointer',
-                    section === 'accounts'
-                        ? 'bg-primary text-primary-foreground'
-                        : 'hover:bg-accent hover:text-accent-foreground'
-                )" @click="section = 'accounts'">
-                    <Fingerprint class="mt-0.5 h-5 w-5 shrink-0" />
-                    <div class="flex-1 space-y-0.5">
-                        <div class="flex items-center gap-2">
-                            <span class="font-medium">
-                                Users Management
-                            </span>
-                        </div>
-                    </div>
-                </button>
+  <div class="flex flex-col gap-8 lg:flex-row">
+    <nav class="w-full lg:w-64">
+      <div class="space-y-1">
+        <button :class="cn(
+          'flex w-full items-start gap-3 rounded-lg px-4 py-3 text-left transition-colors hover:cursor-pointer',
+          section === 'webauth'
+            ? 'bg-primary text-primary-foreground'
+            : 'hover:bg-accent hover:text-accent-foreground'
+        )" @click="section = 'webauth'">
+          <Fingerprint class="mt-0.5 h-5 w-5 shrink-0" />
+          <div class="flex-1 space-y-0.5">
+            <div class="flex items-center gap-2">
+              <span class="font-medium">
+                Web Authentication
+              </span>
             </div>
-        </nav>
+          </div>
+        </button>
+        <button v-if="isAdmin" :class="cn(
+          'flex w-full items-start gap-3 rounded-lg px-4 py-3 text-left transition-colors hover:cursor-pointer',
+          section === 'accounts'
+            ? 'bg-primary text-primary-foreground'
+            : 'hover:bg-accent hover:text-accent-foreground'
+        )" @click="section = 'accounts'">
+          <Users class="mt-0.5 h-5 w-5 shrink-0" />
+          <div class="flex-1 space-y-0.5">
+            <div class="flex items-center gap-2">
+              <span class="font-medium">
+                Users Management
+              </span>
+            </div>
+          </div>
+        </button>
+      </div>
+    </nav>
 
-        <div class="flex-1">
-            <settings-admin-users v-if="section === 'accounts' && isAdmin" />
-            <settings-webauth v-else-if="section === 'webauth'" />
-        </div>
+    <div class="flex-1">
+      <settings-admin-users v-if="section === 'accounts' && isAdmin" />
+      <settings-webauth v-else-if="section === 'webauth'" />
     </div>
+  </div>
 
 </template>
