33fe6947a90ae754e1d04c2c722ae9c4e463f130
alexchoicy <alex123321choicy@gmail.com>
2026-02-21 00:32:54 +0800

    test

--- apps/web/src/lib/ServerFunction/checkBotHeader.ts
+++ apps/web/src/lib/ServerFunction/checkBotHeader.ts
@@ -4,6 +4,8 @@ import { getRequestHeader } from "@tanstack/react-start/server";
 export const checkBotHeader = createServerFn().handler(async () => {
 	const userAgent = getRequestHeader("user-agent") || "";
 
+	console.log("User-Agent:", userAgent);
+
 	const botUserAgents = ["Discordbot"];
 
 	return botUserAgents.some((botAgent) => userAgent.includes(botAgent));
