5ab5dc041ac063f8ed76a2c9045a511ce7737a41
alexchoicy <alex123321choicy@gmail.com>
2026-02-25 02:17:10 +0800

    this header is better

--- apps/web/src/components/ui/appHeader.tsx
+++ apps/web/src/components/ui/appHeader.tsx
@@ -3,14 +3,12 @@ import { SidebarTrigger } from "../shadcn/sidebar";
 export function AppHeader({ children }: { children?: React.ReactNode }) {
 	return (
 		<div className="sticky top-0 z-50 bg-background border-b">
-			<header className="flex h-16 shrink-0 items-center gap-2 px-4">
-				<div className="flex justify-between w-full">
-					<div className="flex items-center gap-2">
-						<SidebarTrigger className="-ml-1" />
-						<h1 className="text-lg font-bold">Music</h1>
-					</div>
-					<div className="flex items-center gap-2">{children}</div>
+			<header className="grid grid-cols-3 h-16 shrink-0 items-center gap-2 px-4">
+				<div className="flex items-center gap-2">
+					<SidebarTrigger className="-ml-1" />
+					<h1 className="text-lg font-bold">Music</h1>
 				</div>
+				{children}
 			</header>
 		</div>
 	);
