d956a29550cad2829393e8aff993805a7eaeb836
alexchoicy <alex123321choicy@gmail.com>
2026-03-13 02:21:02 +0800

    audioPlayer optimization

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
