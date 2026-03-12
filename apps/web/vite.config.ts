19e06fa9fd276f695cd5aee9143f514c0920ad78
alexchoicy <alex123321choicy@gmail.com>
2026-03-12 21:10:23 +0800

    use react compiler and hope it work fine

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
@@ -22,7 +23,11 @@ const config = defineConfig({
 		}),
 		tailwindcss(),
 		tanstackStart(),
-		viteReact(),
+		viteReact({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
 	],
 });
 
