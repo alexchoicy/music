91e82a66d93a1cc7d37554b4695e9aaa9fb21a1b
alexchoicy <alex123321choicy@gmail.com>
2026-09-14 23:04:36 +0800

    web pwa manifest

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -35,6 +35,14 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 		],
 		links: [
 			{
+				rel: "manifest",
+				href: "/manifest.json",
+			},
+			{
+				rel: "icon",
+				href: "/favicon.ico",
+			},
+			{
 				rel: "stylesheet",
 				href: appCss,
 			},
